f521b11c3ad66da9cdf4ed5f4aafd4675a5c5133
Mika Nokka <mika.nokka1@gmail.com>
2017-04-07 22:05:45 +0200

    First working version

--- lister.py
+++ lister.py
@@ -1,7 +1,17 @@
+
+## Quick hack 7.4.2017 mika.nokka1@gmail.com
+#Find mp4 files, use converter for mp3 transformation
+#Windows variant
+
+
 import glob
+import os
+
 list= glob.glob("*.mp4")
 
 for items in list:
     mp3=items.replace(".mp4", ".mp3")
-    print("ffmpeg -i "+items +" "+mp3)
+    command="ffmpeg -i \""+items+"\"  \""+mp3+"\""
+    print("****** EXECUTING:" + command)
+    os.system(command)
     #ffmpeg -i input.mp4 output.avi
