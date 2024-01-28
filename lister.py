af4a5089b58124f7f066922fb627106dccb0c362
Mika Nokka <mika.nokka1@gmail.com>
2024-01-28 13:27:51 +0200

    Add status raporting

--- lister.py
+++ lister.py
@@ -1,17 +1,38 @@
 
-## Quick hack 7.4.2017 mika.nokka1@gmail.com
-#Find mp4 files, use converter for mp3 transformation
-#Windows variant
+# Quick hack 7.4.2017 mika.nokka1@gmail.com, Jan 2024
+# Find mp4 files, use converter for mp3 transformation, show stats
+ 
 
 
 import glob
 import os
+import time
 
 list= glob.glob("*.mp4")
+amount=len(list)
+print(f"===> Found {amount} mp4 files")
+print (f"Files: {list}")
+print (f"--------------------------------------------------------------")
 
+
+start_time = time.time()
+counter=0
 for items in list:
+    counter=counter+1
     mp3=items.replace(".mp4", ".mp3")
     command="ffmpeg -n -i \""+items+"\"  \""+mp3+"\""
-    print("****** EXECUTING:" + command)
+    print(f"****************************************************************")
+    print(f"*  PROGRESS: {counter}/{amount}")
+    print(f"*  FILE: {items}")
+    print(f"*  EXECUTING:" + command)
+    print (f"****************************************************************")
     os.system(command)
     #ffmpeg -i input.mp4 output.avi
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+minutes = int(elapsed_time // 60)
+seconds = int(elapsed_time % 60)
+
+print(f"===> Processed {amount} mp4 files")
+print(f"===> Time taken: {minutes} minutes and {seconds} seconds")
